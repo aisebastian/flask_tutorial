c7e44f2c0cb4785917553d587deb727a114d18e4
Sebastian Irimia <sebatian.irimia@gmail.com>
2018-09-04 16:51:26 +0300

    Added http info handler

--- todo/app.py
+++ todo/app.py
@@ -1,5 +1,6 @@
 
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 from flask import Flask
 import os
 
@@ -12,3 +13,25 @@ db = SQLAlchemy(app)
 def hello_world():
     """Print 'Hello, world!' as the response body."""
     return 'Hello, world!'
+
+
+@app.route('/api/v1', methods=["GET"])
+@app.route('/api/v1/', methods=["GET"])
+def info_view():
+    """List of routes for this API."""
+    output = {
+        'info': 'GET /api/v1',
+        'register': 'POST /api/v1/accounts',
+        'single profile detail': 'GET /api/v1/accounts/<username>',
+        'edit profile': 'PUT /api/v1/accounts/<username>',
+        'delete profile': 'DELETE /api/v1/accounts/<username>',
+        'login': 'POST /api/v1/accounts/login',
+        'logout': 'GET /api/v1/accounts/logout',
+        "user's tasks": 'GET /api/v1/accounts/<username>/tasks',
+        "create task": 'POST /api/v1/accounts/<username>/tasks',
+        "task detail": 'GET /api/v1/accounts/<username>/tasks/<id>',
+        "task update": 'PUT /api/v1/accounts/<username>/tasks/<id>',
+        "delete task": 'DELETE /api/v1/accounts/<username>/tasks/<id>'
+    }
+    return jsonify(output)
+
