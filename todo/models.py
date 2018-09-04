1e741a060642f497ea39b5172124748aa61ed6d4
Sebastian Irimia <sebatian.irimia@gmail.com>
2018-09-04 16:42:56 +0300

    Created User model

--- todo/models.py
+++ todo/models.py
@@ -14,3 +14,19 @@ class Task(db.Model):
         """On construction, set date of creation."""
         super().__init__(*args, **kwargs)
         self.creation_date = datetime.now()
+
+
+class User(db.Model):
+    """The User object that owns tasks."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode, nullable=False)
+    email = db.Column(db.Unicode, nullable=False)
+    password = db.Column(db.Unicode, nullable=False)
+    date_joined = db.Column(db.DateTime, nullable=False)
+    token = db.Column(db.Unicode, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """On construction, set date of creation."""
+        super().__init__(*args, **kwargs)
+        self.date_joined = datetime.now()
+        self.token = secrets.token_urlsafe(64)
