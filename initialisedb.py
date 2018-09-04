31aa026319ac4fe32f30487fba20b3481e6b96e3
Sebastian Irimia <sebatian.irimia@gmail.com>
2018-09-04 17:41:14 +0300

    Initialised a default user

--- initialisedb.py
+++ initialisedb.py
@@ -1,7 +1,18 @@
 from todo.app import db
+from todo.models import User, Task
 import os
 
 if bool(os.environ.get('DEBUG', '')):
     db.drop_all()
 
 db.create_all()
+
+user = User()
+user.username="sebastian"
+user.email="sebastian@test.local"
+user.password="thisshouldnotbeplaintext"
+
+
+db.session.add(user)
+db.session.commit()
+
