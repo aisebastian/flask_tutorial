ec5b4a60c7dbe8e273e45e845e3c17359dcee1ef
Sebastian Irimia <sebatian.irimia@gmail.com>
2018-09-04 16:42:38 +0300

    Created Task model

---
+++ todo/models.py
@@ -0,0 +1,16 @@
+from .app import db
+from datetime import datetime
+
+class Task(db.Model):
+    """Tasks for the To Do list."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, nullable=False)
+    note = db.Column(db.Unicode)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    due_date = db.Column(db.DateTime)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, *args, **kwargs):
+        """On construction, set date of creation."""
+        super().__init__(*args, **kwargs)
+        self.creation_date = datetime.now()
