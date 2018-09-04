403e26c2481a83b48c61fdd5eb607b02fd494b4b
Sebastian Irimia <sebatian.irimia@gmail.com>
2018-09-04 17:13:31 +0300

    Added task creation handler

--- todo/app.py
+++ todo/app.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 from flask import Flask
@@ -7,6 +7,8 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
+
+INCOMING_DATE_FMT = '%d/%m/%Y %H:%M:%S'
 
 @app.route('/')
 def hello_world():
@@ -34,3 +36,26 @@ def info_view():
     }
     return jsonify(output)
 
+
+@app.route('/api/v1/accounts/<username>/tasks', methods=['POST'])
+def create_task(username):
+    """Create a task for one user."""
+    user = User.query.filter_by(username=username).first()
+    if user:
+        task = Task(
+            name=request.form['name'],
+            note=request.form['note'],
+            creation_date=datetime.now(),
+            due_date=datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None,
+            completed=bool(request.form['completed']),
+            user_id=user.id,
+        )
+        db.session.add(task)
+        db.session.commit()
+        output = {'msg': 'posted'}
+        response = Response(
+            mimetype="application/json",
+            response=json.dumps(output),
+            status=201
+        )
+        return response
